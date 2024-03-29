6c4f5b0d7433b99241c786dc03c495ea0d4ef97e
Daniel Farynski <danielfarynski99@gmail.com>
2024-03-29 14:41:43 +1100

    Desktop-down @media queries finished, ready for deployment.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,9 +9,6 @@ export default function App(){
 
         <TechStack/>
         
-        <svg id={'line'} width="100%" height="100%" viewBox="0 0 800 600" xmlns="http://www.w3.org/2000/svg">
-            <path d="M600,2 Q600,110 500,110 T400,285" stroke="grey" fill="none" stroke-width="2"/>
-        </svg>
 
 
     </div>
