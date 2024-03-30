2fe16c36b432b0dd05554edd188e4a691892ed90
Daniel Farynski <danielfarynski99@gmail.com>
2024-03-30 20:24:46 +1100

    - Added 10% threshold to Tech Stack as Tech Stack was executing slightly before About Me - Fixed a small glitch in footer for mobile

--- src/components/About/TechStack.js
+++ src/components/About/TechStack.js
@@ -6,14 +6,24 @@ export default function AboutMe({ sectionTitle }) {
   const domRef = useRef();
 
   useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          setIsVisible(true);
-          observer.unobserve(entry.target); // Stop observing the target after it becomes visible
+    const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              setIsVisible(true);
+              observer.unobserve(entry.target); // Stop observing the target
+            }
+          });
+        },
+        {
+          // Root margin can be adjusted as needed, e.g., "-100px" delays the trigger
+          // until the target is well within the viewport. Play with this value.
+          rootMargin: '-100px',
+          // Threshold set to 0.1 means the callback will trigger when 10%
+          // of the target is visible. Adjust based on your needs.
+          threshold: 0.1
         }
-      });
-    });
+    );
 
     const currentRef = domRef.current;
     if (currentRef) {
