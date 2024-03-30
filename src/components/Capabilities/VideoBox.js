7b27344c3fbffaef7796d6b429d4a5d1d3e1c847
Daniel Farynski <danielfarynski99@gmail.com>
2024-03-30 19:35:44 +1100

    - Added 'playsInline' line to prevent auto-fullscreen on iOS devices for videos

--- src/components/Capabilities/VideoBox.js
+++ src/components/Capabilities/VideoBox.js
@@ -1,10 +1,22 @@
-export default function VideoBox({id, src}) {
-    return (
-        <div id={id}>
-            <video id='video' style={{ width: '100%', height: '100%', objectFit: 'cover', objectPosition: 'center' }} autoPlay muted loop>
-                <source src={src} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
-        </div>
-    );
+export default function VideoBox({ id, src }) {
+  return (
+    <div id={id}>
+      <video
+        id="video"
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          objectPosition: "center",
+        }}
+        autoPlay
+        muted
+        loop
+        playsInline
+      >
+        <source src={src} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
 }
